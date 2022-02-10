1ab4cee630d42616f2571bff36096582592448b4
John Hur <hur.john628@gmail.com>
2022-02-10 00:56:47 -0600

    added thoughtSchema to Thought.js

---
+++ models/Thought.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => date.toLocaleDateString()
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
