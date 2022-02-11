721d193fa4c365e3d0b75b3969d1aab116009724
John Hur <hur.john628@gmail.com>
2022-02-10 20:37:25 -0600

    updated connection.js and added some code to data.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost/socialNetwork', {
+connect('mongodb://localhost/socialDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
---
+++ util/data.js
@@ -0,0 +1,36 @@
+const usernames = [
+    "fakeguy99",
+    "realguy77",
+    "okayguy70",
+    "whynotgirl11",
+    "gurlzrule88",
+    "fakegirl55"
+];
+
+const emails = [
+    "fakeguy99@qmail.com",
+    "realguy77@qmail.com",
+    "okayguy70@qmail.com",
+    "whynotgirl11@qmail.com",
+    "gurlzrule88@qmail.com",
+    "fakegirl55@qmail.com"
+];
+
+const thoughts = [
+    "I think Henry is a real jerk! What a jerk!",
+    "... NO MORE PUMPKIN PIES!",
+    "It's so easy to come up with thoughts. PSYCH!",
+    "CHANNING TATUM IS MINE! I'LL MARRY HIM!",
+    "IONCDSLKBHUIEBHKLJLSKDFJIOIOJ!",
+    "Sam is handsome. Aidan is smart. Kendall is cool."
+];
+
+const reactions = [
+    "Whoa... No way.",
+    "YOU'RE WRONG! FAKE NEWS!",
+    "WHY?!",
+    "Oh no. What happened?",
+    "lolz",
+    "STFU!"
+];
+
