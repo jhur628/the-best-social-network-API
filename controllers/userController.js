f66941209c9056b1e5786ed624efb08003218b78
John Hur <hur.john628@gmail.com>
2022-02-11 20:33:05 -0600

    able to update user by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,5 +34,18 @@ module.exports = {
             console.log(err);
             return res.status(500).json(err);
         });
+    },
+    updateUserById(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId},
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((user) => 
+                !user
+                    ? res.status(404).json({ message: 'No user with this id!' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
     }
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,11 +3,12 @@ const router = require('express').Router();
 const {
     createUser,
     getUsers,
-    getUserById
+    getUserById,
+    updateUserById
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
 
-router.route('/:userId').get(getUserById)
+router.route('/:userId').get(getUserById).put(updateUserById)
 
 module.exports = router;
