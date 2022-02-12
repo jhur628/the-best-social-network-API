c09d4268714858ec09d6fcf198bdb4c67b3d8bce
John Hur <hur.john628@gmail.com>
2022-02-11 20:18:55 -0600

    able to get user by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,18 +10,29 @@ module.exports = {
     // GET all users
     getUsers(req, res) {
         User.find({})
+        .select('-__v')
         .populate('thoughts')
         .populate('friends')
-        .select('-__v')
         .then(users => res.json(users))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         });
     },
-    // // Get user by id
-    // getUserById(req, res) {
-    //     User.findOne({ _id:req.params.userId })
-    // }
-
+    // Get user by id
+    getUserById(req, res) {
+        User.findOne({ _id:req.params.userId })
+        .select('-__v')
+        .populate('thoughts')
+        .populate('friends')
+        .then(async (user) =>
+            !user
+                ? res.status(404).json({ message: 'No user with that ID!' })
+                : res.json(user)
+        )
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+        });
+    }
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 
 const {
     createUser,
-    getUsers
+    getUsers,
+    getUserById
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get(getUserById)
 
 module.exports = router;
